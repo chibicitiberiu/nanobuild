6cceca47742abc316a123e49fabe246e4bc8db36
Tiberiu Chibici <chibicitiberiu@gmail.com>
2021-09-11 22:32:12 +0300

    Added import_file method.

--- src/nanobuild/__init__.py
+++ src/nanobuild/__init__.py
@@ -1,4 +1,5 @@
 import os
+import importlib.util
 
 from .builder import *
 from .environment import Environment
@@ -9,3 +10,11 @@ from .utility import Utility
 
 def run(*targets, environ=os.environ):
     Nanobuild().run(*targets, environ=environ)
+
+
+def import_file(file_name):
+    """Imports given file as a module, and returns the module."""
+    spec = importlib.util.spec_from_file_location("module.name", "file_name")
+    imported_module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(imported_module)
+    return imported_module
